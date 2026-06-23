efeb3122fa38f968c962e43d17224074392d7e8a
Cecilia <cecilia92fernandez@gmail.com>
2026-06-23 00:03:06 -0300

    Se agrego la validación de la verificación

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -1,10 +1,12 @@
 <?php
-class LoginController{
+class LoginController
+{
     private $model;
     private $renderer;
     private $request;
 
-    public function __construct($model, $renderer, $request){
+    public function __construct($model, $renderer, $request)
+    {
         $this->model    = $model;
         $this->renderer = $renderer;
         $this->request  = $request;
@@ -25,26 +27,34 @@ class LoginController{
 
         $this->renderer->render("loginView", $data);
     }
-    public function autenticar(){
-        $usuario= $this->request->post("usuario");
-        $contrasenia= $this->request->post("contrasenia");
+    public function autenticar()
+    {
+        $usuario = $this->request->post("usuario");
+        $contrasenia = $this->request->post("contrasenia");
 
-        if(empty($usuario) || empty($contrasenia)){
-            $this->renderer->render("loginView",[
-                "error" =>"campos vacios"
+        if (empty($usuario) || empty($contrasenia)) {
+            $this->renderer->render("loginView", [
+                "error" => "campos vacios"
             ]);
             return;
         }
-        $user= $this->model->getUsuarioByName($usuario);
+        $user = $this->model->getUsuarioByName($usuario);
 
-        if($user==null || !password_verify($contrasenia,$user["contrasena"])){
-            $this->renderer->render("loginView",[
-                "error" =>"usuario o contraseña incorrectos"
+        if ($user == null || !password_verify($contrasenia, $user["contrasena"])) {
+            $this->renderer->render("loginView", [
+                "error" => "usuario o contraseña incorrectos"
             ]);
             return;
         }
-        $_SESSION["id"]=$user["id"];
-        $_SESSION["usuario"]=$user["nombre_usuario"];
+
+        if ($user["verificado"] == 0) {
+            $this->renderer->render("loginView", [
+                "error" => "Debes verificar tu correo antes de iniciar sesión"
+            ]);
+            return;
+        }
+        $_SESSION["id"] = $user["id"];
+        $_SESSION["usuario"] = $user["nombre_usuario"];
 
 
 
@@ -53,11 +63,10 @@ class LoginController{
         exit;
     }
     public function logout()
-{
-    session_destroy();
+    {
+        session_destroy();
 
-    header("Location:/login/irAlLogin");
-    exit;
-}
-
+        header("Location:/login/irAlLogin");
+        exit;
+    }
 }
