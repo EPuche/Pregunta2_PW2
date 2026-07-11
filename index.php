2e8e3774d6b3de43147b90db648cea1484d831b2
Valentina <valenvdz7@gmail.com>
2026-07-11 02:20:20 -0300

    modificando index para que al crear la sesion se mantenga las cookies en caso de navegar por fuera del sitio(mp)

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
 require_once __DIR__ . '/helpers/Autoloader.php';
 require_once __DIR__ . '/vendor/autoload.php';
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_set_cookie_params([
+        'lifetime' => 0,
+        'path' => '/',
+        'domain' => '',
+        'secure' => true,
+        'httponly' => true,
+        'samesite' => 'None',
+    ]);
+    session_start();
+}
+
 $config = new Configurator();
 $router = $config->getRouter();
 
